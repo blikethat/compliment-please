453ed2f739870c50726dba74e9baa4233afc6882
blikethat <kamie.lyn@gmail.com>
2016-05-14 01:26:03 -0700

    initial commit

--- server.js
+++ server.js
@@ -1,18 +1,32 @@
-var twilio = require('twilio'),
-client = twilio('cfg.accountSid', 'cfg.authToken'),
-var express = require('express'),
-bodyParser = require('body-parser'),
+var twilio = require('twilio');
+if(!process.env.PRODUCTION){
+  var whatever = require('./shhh.js');
+  process.env.TWILIO_ACCOUNT_SID = whatever.TWILIO_ACCOUNT_SID;
+  process.env.TWILIO_AUTH_TOKEN = whatever.TWILIO_AUTH_TOKEN;
+  process.env.TWILIO_NUMBER = whatever.TWILIO_NUMBER;
+}
+
+var client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+var express = require('express');
+bodyParser = require('body-parser');
 app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+app.use('/', express.static(__dirname + '/public'));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.post('/inbound',function(req, res){
   req.body;
   client.sendSms({
-      to:'+15597089066',
-      from:'19492983824',
+      to: req.body.to,
+      from: process.env.TWILIO_NUMBER,
       body:'Your kindness warms the room like a space heater in an ice cave'
   }, function(error, message) {
       if (!error) {
@@ -25,4 +39,8 @@ app.post('/inbound',function(req, res){
           console.log(error);
       }
   });
+});
+
+app.listen(3000, function(){
+  console.log('listening on 3000');
 });
